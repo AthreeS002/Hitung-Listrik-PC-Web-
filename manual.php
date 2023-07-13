469187124145b5b877cb8d2d49b46374b6c9d249
Envy Noxious <adliwibuu135@gmail.com>
2023-07-13 20:54:14 +0700

    function manual

--- manual.php
+++ manual.php
@@ -1,136 +1,7 @@
 <?php
 include("koneksi.php");
+include('header.php');
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Manual Calculator</title>
-
-    <!-- Font Awesome Icons -->
-    <link rel="stylesheet" href="https://kit.fontawesome.com/f8b26cae5f.css" crossorigin="anonymous">
-    <link rel="stylesheet" href="admin/plugins/fontawesome-free/css/all.min.css">
-    <!-- overlayScrollbars -->
-    <link rel="stylesheet" href="admin/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
-    <!-- Theme style -->
-    <link rel="stylesheet" href="admin/dist/css/adminlte.min.css">
-    <!-- Google Font: Source Sans Pro -->
-    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
-</head>
-<body class="hold-transition sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
-    <div class="wrapper">
-
-    <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-      <!-- Left navbar links -->
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-        </li>
-        <!-- <li class="nav-item">
-          <a class="nav-link" href="">
-            Hai, <?= $nama; ?>
-          </a>
-        </li> -->
-      </ul>
-
-      <!-- Right navbar links -->
-      <ul class="navbar-nav ml-auto">
-        <!-- coolers Dropdown Menu -->
-        <li class="nav-item">
-          <a class="nav-link" href="login.php" onclick="return confirm('Apakah Anda Yakin Ingin Keluar?')">
-            Login &nbsp;
-            <i class="fas fa-sign-out-alt"></i>
-          </a>
-        </li>
-      </ul>
-    </nav>
-    <!-- /.navbar -->
-
-    <!-- Main Sidebar Container -->
-    <aside class="main-sidebar sidebar-dark-primary elevation-4">
-      <!-- Brand Logo -->
-      <a href="#" class="brand-link">
-        <i class="fas fa-hotel img-circle elevation-3 ml-3"></i>
-        &nbsp;
-        <span class="brand-text font-weight-bold">PC Builder</span>
-      </a>
-
-      <!-- Sidebar -->
-      <div class="sidebar">
-
-        <!-- Sidebar Menu -->
-        <div class="mt-3">
-          <nav class="mt-2">
-            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-              <!-- Add icons to the links using the .nav-icon class
-               with font-awesome or any other icon font library -->
-              <li class="nav-item has-treeview">
-                <a href="index.php" class="nav-link">
-                  <i class="nav-icon fas fa-home"></i>
-                  <p>
-                    Dashboard
-                  </p>
-                </a>
-              </li>
-              <li class="nav-header">DATA MASTER</li>
-              <li class="nav-item">
-                <a href="gpu.php" class="nav-link">
-                  <i class="nav-icon fas fa-person-booth"></i>
-                  <p>
-                    Graphic Card
-                  </p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="procie.php" class="nav-link">
-                  <i class="nav-icon fas fa-users"></i>
-                  <p>
-                    Prosesor
-                  </p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="motherboard.php" class="nav-link">
-                  <i class="nav-icon fas fa-users"></i>
-                  <p>
-                    Motherboard
-                  </p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="ram.php" class="nav-link">
-                  <i class="nav-icon fas fa-users"></i>
-                  <p>
-                    RAM
-                  </p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="#" class="nav-link active">
-                  <i class="nav-icon fas fa-user"></i>
-                  <p>
-                    Manual
-                  </p>
-                </a>
-              </li>
-              <li class="nav-header">DATA ADMIN</li>
-              <li class="nav-item">
-                <a href="admin/index.php" class="nav-link">
-                  <i class="nav-icon fas fa-user-shield"></i>
-                  <p>
-                    Administrator
-                  </p>
-                </a>
-              </li>
-            </ul>
-          </nav>
-        </div>
-        <!-- /.sidebar-menu -->
-      </div>
-      <!-- /.sidebar -->
-    </aside>
 
     <!-- Content Wrapper. Contains page content -->
     <div class="content-wrapper">
@@ -165,37 +36,43 @@ include("koneksi.php");
 											<div class="col-md-6">
 												<div class="form-group">
 													<label class="label" for="procie">Processor</label>
-													<input type="text" class="form-control" name="procie" id="procie" placeholder="Type in watt" required>
+													<input type="number" class="form-control" name="procie" id="procie" placeholder="Type in watt" required>
 												</div>
 											</div>
 											<div class="col-md-6"> 
 												<div class="form-group">
 													<label class="label" for="gpu">Graphic Card</label>
-													<input type="text" class="form-control" name="gpu" id="gpu" placeholder="Type in watt" required>
+													<input type="number" class="form-control" name="gpu" id="gpu" placeholder="Type in watt" required>
 												</div>
 											</div>
 											<div class="col-md-6">
 												<div class="form-group">
 													<label class="label" for="mobo">Motherboard</label>
-													<input type="text" class="form-control" name="mobo" id="mobo" placeholder="Type in watt" required>
+													<input type="number" class="form-control" name="mobo" id="mobo" placeholder="Type in watt" required>
 												</div>
 											</div>
 											<div class="col-md-6">
 												<div class="form-group">
 													<label class="label" for="cooler">Cooler</label>
-													<input type="text" class="form-control" name="cooler" id="cooler" placeholder="Type in watt" required>
+													<input type="number" class="form-control" name="cooler" id="cooler" placeholder="Type in watt" required>
 												</div>
 											</div>
-                                            <div class="col-md-12">
+                      <div class="col-md-6">
 												<div class="form-group">
 													<label class="label" for="monitor">Monitor</label>
-													<input type="text" class="form-control" name="monitor" id="monitor" placeholder="Type in watt" required>
+													<input type="number" class="form-control" name="monitor" id="monitor" placeholder="Type in watt" required>
 												</div>
 											</div>
-                                            <div class="col-md-12">
+                      <div class="col-md-6">
 												<div class="form-group">
 													<label class="label" for="ram">RAM</label>
-													<input type="text" class="form-control" name="ram" id="ram" placeholder="Type in watt" required>
+													<input type="number" class="form-control" name="ram" id="ram" placeholder="Type in watt" required>
+												</div>
+											</div>
+                      <div class="col-md-6">
+												<div class="form-group">
+													<label class="label" for="ram">Penggunaan per hari</label>
+													<input type="number" class="form-control" name="penggunaan" id="penggunaan" placeholder="Type in hour" required>
 												</div>
 											</div>
 											<div class="col-md-12">
@@ -208,6 +85,7 @@ include("koneksi.php");
 									</form>
 								</div>
 							</div>
+
 							<div class="col-lg-4 col-md-5 d-flex align-items-stretch">
 								<div class="info-wrap bg-primary w-100 p-md-5 p-4">
 									<h3>Let's get in touch</h3>
@@ -225,7 +103,7 @@ include("koneksi.php");
 				        			<span class="fa fa-phone"></span>
 				        		</div>
 				        		<div class="text pl-3">
-					            <p><span>Phone:</span> <a href="tel://1234567920">+ 1235 2355 98</a></p>
+					            <p><span>Phone:</span> + 1235 2355 98</p>
 					          </div>
 				          </div>
 				        	<div class="dbox w-100 d-flex align-items-center">
--- result_manual.php
+++ result_manual.php
@@ -1,145 +1,33 @@
 <?php
-$procie = $_POST['procie'];
-$gpu = $_POST['gpu'];
-$mobo = $_POST['mobo'];
-$cooler = $_POST['cooler'];
-$monitor = $_POST['monitor'];
-$ram = $_POST['ram'];
-$total = $procie + $gpu + $mobo + $cooler + $monitor + $ram;
+function calculate($procie, $gpu, $mobo, $cooler, $monitor, $ram, $penggunaan){
+  $total = ($procie + $gpu + $mobo + $cooler + $monitor + $ram) /1000;
+  $harian = ($total * $penggunaan) * 605;
+  $hasil = $harian * 30;
+
+  return [
+    'total' => $total,
+    'harian' => $harian,
+    'hasil' => $hasil
+  ];
+}
+
+$procie = floatval($_POST['procie']);
+$gpu = floatval($_POST['gpu']);
+$mobo = floatval($_POST['mobo']);
+$cooler = floatval($_POST['cooler']);
+$monitor = floatval($_POST['monitor']);
+$ram = floatval($_POST['ram']);
+$penggunaan = floatval($_POST['penggunaan']);
+
+$result = calculate($procie, $gpu, $mobo, $cooler, $monitor, $ram, $penggunaan);
+$total = $result['total'];
+$harian = $result['harian'];
+$hasil = $result['hasil'];
+
 
 //$total = ($procie + $gpu + $mobo + $cooler + $monitor + $ram);
-
+include('header.php');
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Manual Calculator</title>
-
-    <!-- Font Awesome Icons -->
-    <link rel="stylesheet" href="https://kit.fontawesome.com/f8b26cae5f.css" crossorigin="anonymous">
-    <link rel="stylesheet" href="admin/plugins/fontawesome-free/css/all.min.css">
-    <!-- overlayScrollbars -->
-    <link rel="stylesheet" href="admin/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
-    <!-- Theme style -->
-    <link rel="stylesheet" href="admin/dist/css/adminlte.min.css">
-    <!-- Google Font: Source Sans Pro -->
-    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
-</head>
-<body class="hold-transition sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
-    <div class="wrapper">
-
-    <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-      <!-- Left navbar links -->
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-        </li>
-        <!-- <li class="nav-item">
-          <a class="nav-link" href="">
-            Hai, <?= $nama; ?>
-          </a>
-        </li> -->
-      </ul>
-
-      <!-- Right navbar links -->
-      <ul class="navbar-nav ml-auto">
-        <!-- coolers Dropdown Menu -->
-        <li class="nav-item">
-          <a class="nav-link" href="login.php" onclick="return confirm('Apakah Anda Yakin Ingin Keluar?')">
-            Login &nbsp;
-            <i class="fas fa-sign-out-alt"></i>
-          </a>
-        </li>
-      </ul>
-    </nav>
-    <!-- /.navbar -->
-
-    <!-- Main Sidebar Container -->
-    <aside class="main-sidebar sidebar-dark-primary elevation-4">
-      <!-- Brand Logo -->
-      <a href="#" class="brand-link">
-        <i class="fas fa-hotel img-circle elevation-3 ml-3"></i>
-        &nbsp;
-        <span class="brand-text font-weight-bold">PC Builder</span>
-      </a>
-
-      <!-- Sidebar -->
-      <div class="sidebar">
-
-        <!-- Sidebar Menu -->
-        <div class="mt-3">
-          <nav class="mt-2">
-            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-              <!-- Add icons to the links using the .nav-icon class
-               with font-awesome or any other icon font library -->
-              <li class="nav-item has-treeview">
-                <a href="index.php" class="nav-link">
-                  <i class="nav-icon fas fa-home"></i>
-                  <p>
-                    Dashboard
-                  </p>
-                </a>
-              </li>
-              <li class="nav-header">DATA MASTER</li>
-              <li class="nav-item">
-                <a href="gpu.php" class="nav-link">
-                  <i class="nav-icon fas fa-person-booth"></i>
-                  <p>
-                    Graphic Card
-                  </p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="procie.php" class="nav-link">
-                  <i class="nav-icon fas fa-users"></i>
-                  <p>
-                    Prosesor
-                  </p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="motherboard.php" class="nav-link">
-                  <i class="nav-icon fas fa-users"></i>
-                  <p>
-                    Motherboard
-                  </p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="ram.php" class="nav-link">
-                  <i class="nav-icon fas fa-users"></i>
-                  <p>
-                    RAM
-                  </p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="#" class="nav-link active">
-                  <i class="nav-icon fas fa-user"></i>
-                  <p>
-                    Manual
-                  </p>
-                </a>
-              </li>
-              <li class="nav-header">DATA ADMIN</li>
-              <li class="nav-item">
-                <a href="admin/index.php" class="nav-link">
-                  <i class="nav-icon fas fa-user-shield"></i>
-                  <p>
-                    Administrator
-                  </p>
-                </a>
-              </li>
-            </ul>
-          </nav>
-        </div>
-        <!-- /.sidebar-menu -->
-      </div>
-      <!-- /.sidebar -->
-    </aside>
 
     <!-- Content Wrapper. Contains page content -->
     <div class="content-wrapper">
@@ -207,10 +95,22 @@ $total = $procie + $gpu + $mobo + $cooler + $monitor + $ram;
 													<input type="text" class="form-control" name="ram" id="ram" value="<?= $ram ?> W" readonly>
 												</div>
 											</div>
-                      <div class="col-md-6">
+                      <div class="col-md-12">
                         <div class="form-group">
                           <label type="text" for="total">Total (Watt)</label>
-                          <input type="text" class="form-control" name="total" value="<?= $total ?> W" readonly>
+                          <input type="text" class="form-control col-md-6" name="total" value="<?= $total*1000 ?> W" readonly>
+                        </div>
+                      </div>
+                      <div class="col-md-6">
+                        <div class="form-group">
+                          <label type="text" for="total">Total Biaya Listrik per Bulan</label>
+                          <input type="text" step="0.01" class="form-control" name="total" value="Rp <?= $hasil ?>,-" readonly>
+                        </div>
+                      </div>
+                      <div class="col-md-6">
+                        <div class="form-group">
+                          <label type="text" for="total">Total Biaya Listrik per Hari</label>
+                          <input type="text" step="0.01" class="form-control" name="total" value="Rp <?= $harian ?>,-" readonly>
                         </div>
                       </div>
 											<div class="col-md-12">
@@ -223,7 +123,11 @@ $total = $procie + $gpu + $mobo + $cooler + $monitor + $ram;
 									</form>
 								</div>
 							</div>
-							<div class="col-lg-4 col-md-5 d-flex align-items-stretch">
+							<!-- END OF RESULT -->
+              
+              
+              <!-- CARD -->
+              <div class="col-lg-4 col-md-5 d-flex align-items-stretch">
 								<div class="info-wrap bg-primary w-100 p-md-5 p-4">
 									<h3>Let's get in touch</h3>
 									<p class="mb-4">We're open for any suggestion or just to have a chat</p>
